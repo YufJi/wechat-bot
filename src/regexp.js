0576dcccb9c90fdfb1ef3d5fd0a498dc7434c94d
ji_yf <ji_yf@ctrip.com>
2019-06-26 18:59:18 +0800

    fix: 修改正则

--- src/regexp.js
+++ src/regexp.js
@@ -1,9 +1,9 @@
 
-const tianqi = /([\w\W]*)[天气|气温|温度]([\w\W]*)/ig;
+const tianqi = /([\w\W]*)(天气|气温|温度)([\w\W]*)/ig;
 
-const joke = /([\w\W]*)[笑话|不开心|难过|伤心]([\w\W]*)/ig;
+const joke = /([\w\W]*)(笑话|不开心|难过|伤心)([\w\W]*)/ig;
 
-const huangli = /([\w\W]*)[黄历]([\w\W]*)/ig;
+const huangli = /([\w\W]*)(黄历)([\w\W]*)/ig;
 
 module.exports = {
   regtianqi: tianqi,
